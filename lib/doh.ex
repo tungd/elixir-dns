7b7144c2d873768fced870c6a29e9ae6c199d043
Nick McKinney <nick@ndmckinney.net>
2019-10-14 13:49:32 -0400

    Move HTTP request to private function

--- lib/doh.ex
+++ lib/doh.ex
@@ -1,10 +1,12 @@
 defmodule DOH do
-  def resolve(domain, type \\ :a, dns_server \\ {"https://dns.google/dns-query", []}) do
-    case query(domain, type, dns_server).anlist do
+  def resolve(domain, type \\ :a, doh_server \\ {"https://dns.google/dns-query", []}) do
+    case query(domain, type, doh_server).anlist do
       answers when is_list(answers) and length(answers) > 0 ->
-        data = answers
-               |> Enum.map(& &1.data)
-               |> Enum.reject(&is_nil/1)
+        data =
+          answers
+          |> Enum.map(& &1.data)
+          |> Enum.reject(&is_nil/1)
+
         {:ok, data}
 
       _ ->
@@ -12,7 +14,7 @@ defmodule DOH do
     end
   end
 
-  def query(domain, type \\ :a, dns_server \\ {"https://dns.google/dns-query", []}) do
+  def query(domain, type \\ :a, doh_server \\ {"https://dns.google/dns-query", []}) do
     record = %DNS.Record{
       header: %DNS.Header{rd: true},
       qdlist: [%DNS.Query{domain: to_charlist(domain), type: type, class: :in}]
@@ -20,14 +22,32 @@ defmodule DOH do
 
     dns_request = DNS.Record.encode(record)
 
-    {http_server, options} = dns_server
-    case HTTPoison.post(http_server, dns_request, [{"Content-Type", "application/dns-message"}], options) do
+    case request(dns_request, doh_server) do
+      {:ok, response} -> response
+      {:error, _} -> %DNS.Record{}
+    end
+  end
+
+  defp request(query, doh_server) do
+    {http_server, options} = doh_server
+
+    case HTTPoison.post(
+           http_server,
+           query,
+           [{"Content-Type", "application/dns-message"}],
+           options
+         ) do
       {:ok, %HTTPoison.Response{status_code: 200, body: response}} ->
-        DNS.Record.decode(response)
-      {:ok, %HTTPoison.Response{status_code: 301}} ->
-          {:error, "moved"}
+        {:ok, DNS.Record.decode(response)}
+
+      {:ok, %HTTPoison.Response{}} ->
+        {:error, "query error"}
+
+      {:error, :fmt} ->
+        {:error, "http error"}
+
       {:error, %HTTPoison.Error{reason: reason}} ->
-          {:error, reason}
-    end  
+        {:error, reason}
+    end
   end
 end
